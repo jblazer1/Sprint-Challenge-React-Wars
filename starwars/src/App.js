8fe947f991a88f3686438ad4026e5a8c5c6be7d0
JamesB <jblazer1@yahoo.com>
2019-08-03 11:56:29 -0700

    Create CharacterList and CharacterCard components - use Styled components for styling

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import CharacterList from "./components/CharacterList";
+import styled from "styled-components";
+
+const Header = styled.h1`
+  font-size: 120px;
+  margin-top: 10px;
+`;
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
     <div className="App">
-      <h1 className="Header">React Wars</h1>
+      <Header>React Wars</Header>
+      <CharacterList />
     </div>
   );
-}
+};
 
 export default App;
